51d960ad3a5129791589d2b66c31e489d915650e
bagley2014 <dbagley@harding.edu>
2024-11-11 01:37:41 -0800

    Support calculating expected value of checks and assigning them tiers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import './mods';
+import { getExpectedValue } from './mods';
 
-function main() {}
+function main() {
+	console.log(getExpectedValue(15, 15));
+	console.log(getExpectedValue(10, 15));
+	console.log(getExpectedValue(20, 20));
+	console.log(getExpectedValue(15, 20));
+}
 
 main();
--- src/mods/index.ts
+++ src/mods/index.ts
@@ -1,6 +1,7 @@
 import attributesJson from './attributes.json';
 import skillItemsJson from './skillItems.json';
 import skillProficienciesJson from './skillProficiencies.json';
+import standardDCsJson from './standardDCs.json';
 import { z } from 'zod';
 
 type Arrayable<T> = T | T[];
@@ -26,7 +27,39 @@ const attributeTierList = ModTierListTable.parse(attributesJson);
 const skillProficiencyTierList = ModTierListTable.parse(skillProficienciesJson);
 const skillItemTierList = ModTierListTable.parse(skillItemsJson);
 
-const level = 2;
+const standardDCs = z.number().array().length(21, 'because there are 20 levels, plus the 0th index').parse(standardDCsJson);
+
+const level = 1;
+
+const valueMultipliers = [-1, 0, 1, 2]; // [0, 0.5, 1, 2] is save-like, rather than rolling
+export function getExpectedValue(mod: Mod, dc: number) {
+	let result = 0;
+	for (let roll = 1; roll <= 20; roll++) {
+		const check = roll + mod + (roll == 1 ? -10 : roll == 20 ? 10 : 0);
+		// crit fail
+		if (check <= dc - 10) result += valueMultipliers[0];
+		// crit success
+		else if (check >= dc + 10) result += valueMultipliers[3];
+		// fail
+		else if (check < dc) result += valueMultipliers[1];
+		// success
+		else result += valueMultipliers[2];
+	}
+	return result / 20;
+}
+
+// Kind of arbitrary tiers; a maxed out first level skill gets A-tier
+function getTier(expectedValue: number): Tier {
+	if (expectedValue >= 1.25) return 'SS';
+	if (expectedValue >= 1) return 'S';
+	if (expectedValue >= 0.75) return 'A';
+	if (expectedValue >= 0.6) return 'B';
+	if (expectedValue >= 0.45) return 'C';
+	if (expectedValue >= 0.3) return 'D';
+	if (expectedValue >= 0.15) return 'F';
+	if (expectedValue > 0) return 'FF';
+	return 'FFF';
+}
 
 function arrayArrayable<T>(val: Arrayable<T>) {
 	if (Array.isArray(val)) return val;
@@ -47,4 +80,10 @@ function multiplyTiers(tierList1: CompositeTierList, tierList2: CompositeTierLis
 
 const t = multiplyTiers(multiplyTiers(attributeTierList[level]!, skillProficiencyTierList[level]!), skillItemTierList[level]!);
 t.sort((a, b) => b[1] - a[1]);
-console.log(t);
+console.log(
+	t.map((x) => [
+		arrayArrayable(x[0]).join('|'),
+		getExpectedValue(x[1], standardDCs[level]),
+		getTier(getExpectedValue(x[1], standardDCs[level])),
+	]),
+);
